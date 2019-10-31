408246763aeb0d3d1b0913d06d49394d979513c2
Brad Smith <brad@smithwebtek.com>
2019-10-31 16:23:50 -0400

    update lab

--- app/javascript/components/UserInfo/index.js
+++ app/javascript/components/UserInfo/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import cs from './styles';
+// import Query from 'graphql';
 
 const UserInfo = () => (
   <div className={cs.userInfo}>👋</div>
+
+  <Query query={Me}>
+    {({ data, loading }) => {
+      if (loading) return "...Loading";
+      if (!data.me) {
+        // Show login form
+        return;
+      }
+
+      const { fullName } = data.me;
+      return <div className={cs.info}>😈 {fullName}</div>;
+    }}
+  </Query>
+
 );
 
 export default UserInfo;
